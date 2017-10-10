a937296cdd3c87b8020139ee9074229f47fe7544
Vu Duc Tuyen <tuyen@naxam.net>
2017-10-10 16:41:19 +0700

    Dropbox Core 3.0.4

---
+++ scripts/classes.cs
@@ -0,0 +1,19 @@
+
+namespace Com.Dropbox.Core.Util {
+    
+}
+
+
+namespace Com.Dropbox.Core.V1 {
+    
+}
+
+
+namespace Com.Dropbox.Core.V2.Paper {
+    
+}
+
+
+namespace Com.Dropbox.Core.V2.Files {
+    
+}
